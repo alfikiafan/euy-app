6cf8fcecea997a1931a8f586be8c7eb4b0449767
Zavier Ferodova <zavierferodova@gmail.com>
2023-12-08 19:03:53 +0700

    Add home route message

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,6 +5,11 @@ const { getUser } = require('../controllers/users')
 const { login, ssoLogin, register, updateAccessToken } = require('../controllers/auth.js')
 
 const router = Router()
+router.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the EUY API'
+  })
+})
 router.get('/recipes', [verifyJwtToken], getRecipes)
 router.get('/recipes/our-choices', [verifyJwtToken], getOurRecipeChoices)
 router.post('/recipes/viewed', [verifyJwtToken], saveViewedRecipe)
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,8 @@ const apiRouter = require('./api.js')
 const router = Router()
 
 router.use('/api', apiRouter)
+router.get('/', (req, res) => {
+  res.redirect('/api')
+})
 
 module.exports = router
