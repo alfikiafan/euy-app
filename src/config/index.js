9252a8d2131eae7ca338917b5cf7adaa5799a78a
Zavier Ferodova <zavierferodova@gmail.com>
2023-12-09 22:31:16 +0700

    Change model api port

--- src/config/index.js
+++ src/config/index.js
@@ -4,5 +4,5 @@ module.exports = {
   appKey: process.env.APP_KEY,
   keySignature: 'euy-data-signature',
   keyExpirationTime: 5 * 60, // expires in 5 minutes
-  modelHostname: 'http://127.0.0.1:5000'
+  modelHostname: 'http://127.0.0.1:5050'
 }
